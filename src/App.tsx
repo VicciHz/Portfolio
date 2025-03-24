0d3818af1e0a239dc2e4e10bd30b0fe9b36877d9
Victor <victor.fox.hz@gmail.com>
2025-03-24 13:03:42 +0100

    initialize React app with Vite and TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <div className="hero-section">
+      <div className="hero-content">
+        <h1>Victor Hernandez</h1>
+        <p>Developer | Musician | Producer</p>
+      </div>
+    </div>
+  );
+}
+
+export default App
